de68b21ead45f6f83049a1f0ba368f4cfa992e81
pdeppen <pdeppen17@aol.com>
2019-02-09 17:53:39 -0500

    #9: Still in progess

--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
+import { Container, Row, Col, ListGroup, ListGroupItem, Button } from 'reactstrap'
 
 class MoviesList extends Component {
+
+    state = {
+        movies:[
+            {
+                movie_title: "Testing Title",
+                id: 1 
+            }
+        ]
+    }
+
     render() {
         return(
-            <h3>Movies List</h3>
+            <Container>
+                <h3>Movies List</h3>
+
+                <ListGroup>
+                        {this.state.movies.map((movie) => (
+                            <ListGroupItem key={movie.id} style={{backgroundColor:"lightgray"}}>
+                                {movie.movie_title}
+                            </ListGroupItem>
+                        ))}
+                </ListGroup>
+            </Container>
         )
     }
 }
